bff0f15c275f2f05a05c8ca69b610a6309b0121e
Thomas Niezyniecki <tmniez@comcast.net>
2023-09-21 10:13:21 -0500

    I added the code for the second step because it deleted: TextField and Button

--- HeaderView.swift
+++ HeaderView.swift
@@ -5,4 +5,21 @@
 //  Created by Thomas Niezyniecki on 9/19/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct MyView: View {
+    @Binding var tasks: [Task]
+    @State var enteredString = ""
+    var body: some View {
+        HStack {
+            TextField("", text: $enteredString)
+            Button {
+                tasks.append(Task(nameOfTask: enteredString))
+            } label: {
+                Text("Add Task")
+            }
+
+        }
+    }
+}
+
